163df875adb89f16ecf6c911b5f001545ef92863
Gustavo de Oliveira <gustavooliveirabrunosilva@gmail.com>
2019-10-06 20:28:29 -0300

    ItemPedido e ItemPedidoPK

--- src/main/java/com/nelioalves/cursomc/domain/enums/TipoCliente.java
+++ src/main/java/com/nelioalves/cursomc/domain/enums/TipoCliente.java
@@ -2,8 +2,8 @@ package com.nelioalves.cursomc.domain.enums;
 
 public enum TipoCliente {
 	
-	PESSOAFISICA(0, "Pessoa Física"),
-	PESSOAJURIDICA(1, "Pessoa Jurídica");
+	PESSOAFISICA(1, "Pessoa Física"),
+	PESSOAJURIDICA(2, "Pessoa Jurídica");
 	
 	private int cod;
 	private String descricao;
